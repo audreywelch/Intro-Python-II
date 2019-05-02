0e8ab89224bcdca9725d17e9d80e207a9a5d224e
audreywelch <welch.audreyl@gmail.com>
2019-05-02 16:20:51 -0500

    Implemented on_take and on_drop methods

--- src/item.py
+++ src/item.py
@@ -5,3 +5,8 @@ class Item(Description):
     def __init__(self, name, description, storage = []):
         super().__init__(name, description, storage = storage)
 
+    def on_take(self.name):
+        print(f'You have picked up {self.name}.')
+
+    def on_drop(self.name):
+        print(f'You have put the {self.name} back.')
