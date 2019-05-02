3d9a5534fcb627d5f40eadfab16c3a8dbddaa7e6
audreywelch <welch.audreyl@gmail.com>
2019-05-02 17:51:28 -0500

    Added inventory printing and changed items to fit the theme

--- src/adv.py
+++ src/adv.py
@@ -1,15 +1,16 @@
 from room import Room
 from player import Player
 from lib import Description
+from item import Item
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Outside Mysterious Entrance",
+                     "North of you, the yellow & blue shimmer beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south.
-Dusty passages run north and east."""),
+    'foyer':    Room("IKEA Foyer", """Dim light filters in from the south.
+Blaring white passages run north and east."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
@@ -18,10 +19,28 @@ the distance, but there is no way across the chasm."""),
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'cafeteria': Room("Treasure Chamber AKA Cafeteria", """You've found the long-lost treasure chamber
+# of juicy swedish meatballs and tart lingonberry sauce! Sadly, all remaining swedish meatballs have
+# already been devoured by earlier adventurers. The only exit is back the way you came. Good luck."""),
 }
+
+item_strings = [
+['\nEktorp-Sofa', 'This couch really seals your committment to that quintessential Scandinavian aesthetic.\n$379'],
+['\nLack-Side-Table', 'Yep, you just scored a side table for less than you spent on lunch yesterday. Nice.\n$7.99'],
+['\nRens-Sheepskin-Rug', 'In case you were wondering whether sheepskin rugs are still trending - the answer is yes.\n$29.99'],
+['\nIngabritta-Throw', 'If that doesn\'t scream pure hygge, I don\'t know what does!\n$24.99'],
+['\nFärgrik-Mug', 'A 99-cent mug doesn\'t need much of an explanation. It holds coffee. It costs a buck.\n$0.99'],
+['\nKryddnejlika', 'Hmmm, nope, we don\'t know what that is either.\n$5.99'],
+['\nFree-Snowman', 'Some assembly required 🤷‍♀️']
+]
+
+items = [Item(item[0], item[1]) for item in item_strings]
+print(items)
+
+room['foyer'].storage = items[0:2]
+room['overlook'].storage = items[2:5]
+room['narrow'].storage = items[5]
+room['treasure'].storage = items[6]
 
 
 # Link rooms together
@@ -54,6 +73,9 @@ directions = ['n', 's', 'e', 'w']
 ## Valid Verb Commands
 actions = ['get', 'drop']
 
+
+
+
 #
 # Main
 #
@@ -70,7 +92,6 @@ while True:
     # * Waits for user input and decides what to do.
     raw_input = input("Where do you want to go? -> ")
     cmd = raw_input.split()
-    print(cmd)
 
     # If user enters ONE word
     ## If the user enters a cardinal direction, attempt to move to the room there.
@@ -80,8 +101,11 @@ while True:
             if new_room is not None:
                 player.current_room = new_room
             else:
-                print("You can't move any farther in that direction.")
+                print("You can't move any farther in that direction - you must stick to the maze- ahem, I mean the path.")
     
+        elif cmd == 'i' or 'inventory':
+            print(f'My shopping cart: {player.storage}')
+        
         elif cmd == 'q':
             print("Goodbye!")
             break
@@ -103,6 +127,9 @@ while True:
                 player.current_room.storage.remove(cmd[1])
                 # and add it to the player's contents
                 player.storage.append(cmd[1])
+
+                # tell user they now have the item
+
             else:
                 print(f'Sorry, there is no {cmd[1]} in this room.')
 
--- src/item.py
+++ src/item.py
@@ -2,11 +2,11 @@ from lib import Description
 
 class Item(Description):
 
-    def __init__(self, name, description, storage = []):
+    def __init__(self, name, description, storage = None):
         super().__init__(name, description, storage = storage)
 
-    def on_take(self.name):
+    def on_take(self):
         print(f'You have picked up {self.name}.')
 
-    def on_drop(self.name):
+    def on_drop(self):
         print(f'You have put the {self.name} back.')
